ec02e8087cb46a7e3c63c8b8e35f99554b3e7854
Unknown <carter@brehmit.com>
2019-02-19 07:36:14 -0600

    made country class for final

---
+++ Final/src/pkgfinal/Country.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgfinal;
+
+/**
+ *
+ * @author carterbrehm
+ */
+public class Country {
+    private final String name;
+    private final int population;
+    
+    public Country(String name, int population) {
+        this.name = name;
+        this.population = population;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+    
+    
+}
