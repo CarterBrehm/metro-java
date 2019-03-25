adc29149752d1731daa6c422326e3dc95b3b243c
Carter Brehm <carter@brehmit.com>
2019-03-25 20:13:53 +0000

    work done on lab 3A

--- BrehmLab3A/src/brehmlab3a/BrehmLab3A.java
+++ BrehmLab3A/src/brehmlab3a/BrehmLab3A.java
@@ -5,6 +5,12 @@
  */
 package brehmlab3a;
 
+import org.w3c.dom.*;
+import org.xml.sax.*;
+import java.io.*;
+import javax.xml.parsers.*;
+import java.text.*;
+
 /**
  *
  * @author carter
@@ -15,7 +21,28 @@ public class BrehmLab3A {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse("cd_catalog.xml");
+
+            NodeList nodelist = document.getElementsByTagName("CD");
+            int num = nodelist.getLength();
+
+            for (int i = 0; i < num; i++) {
+                Node cd = nodelist.item(i);
+                NodeList childNodes = cd.getChildNodes();
+                System.out.println(childNodes.item(1).getTextContent() + childNodes.item(2).getTextContent() + childNodes.item(6).getTextContent());
+            }
+
+        } catch (ParserConfigurationException e) {
+            System.err.print(e);
+        } catch (SAXException e) {
+            System.err.print(e);
+        } catch (IOException e) {
+            System.err.print(e);
+        }
+
     }
-    
+
 }
