d6de05b784e09c73a91e2818a5bae05e29111c44
CarterBrehm <carter@brehmit.com>
2019-04-01 14:41:49 -0500

    Completed Lab 4A and 4B

---
+++ BrehmLab4B/src/brehmlab4b/BrehmLab4B.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package brehmlab4b;
+
+/**
+ *
+ * @author crbrehm
+ */
+public class BrehmLab4B {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        AddressBook abook = new AddressBook();
+        abook.setVisible(true);
+    }
+    
+}
