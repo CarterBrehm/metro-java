76ff150f1e05dc6913ec539ff0f72dbe17a55a74
Carter Brehm <carter@brehmit.com>
2019-05-07 14:16:25 -0500

    work done on Lab 8A

--- BrehmLab8A/src/brehmlab8a/BrehmLab8A.java
+++ BrehmLab8A/src/brehmlab8a/BrehmLab8A.java
@@ -7,11 +7,14 @@ package brehmlab8a;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,6 +23,56 @@ import java.util.logging.Logger;
  * @author crbrehm
  */
 public class BrehmLab8A {
+    
+    public static int findID(ResultSet result) {
+        int highestID = -1;
+        try {
+            while (result.next()) {
+                if (result.getInt("ID") > highestID) {
+                    highestID = result.getInt("ID");
+                }
+            }
+        } catch (SQLException ex) {
+            System.err.println(ex);
+        }
+        return highestID;
+    }
+    
+    public static void addRecord(ResultSet result, Connection dbCon) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList record = new ArrayList();
+        try {
+            System.out.println("NEW RECORD:");
+            System.out.println("ID: " + (findID(result) + 1));
+            record.add(findID(result) + 1);
+            ResultSetMetaData metadata = result.getMetaData();
+            for (int i = 2; i <= metadata.getColumnCount(); i++) {
+                String input = null;
+                do {
+                    System.out.print(metadata.getColumnName(i) + ": ");
+                    input = sc.nextLine();
+                    record.add(input);
+                } while (input.isEmpty());
+                input = null;
+            }
+            PreparedStatement ps = dbCon.prepareStatement("INSERT INTO CONTRACTORS (ID, COMPANYNAME, PHONE, CONTACTNAME, RATING, OUTOFSTATESERVICE) VALUES(?,?,?,?,?,?)");
+            System.out.println(record);
+            ps.setInt(1, (int) record.get(0));
+            ps.setString(2, (String) record.get(1));
+            ps.setString(3, (String) record.get(2));
+            ps.setString(4, (String) record.get(3));
+            ps.setInt(5, (int) record.get(4));
+            ps.setBoolean(6, (boolean) Boolean.parseBoolean((String) record.get(5)));
+            ps.execute();
+            
+        } catch (SQLException ex) {
+            System.err.println(ex);
+        }
+    }
+    
+    public static void delRecord(ResultSet result, int index) {
+        
+    }
 
     public static String getData(ResultSet result) {
         StringBuilder sb = new StringBuilder();
@@ -35,7 +88,7 @@ public class BrehmLab8A {
             System.out.print("Choose a column from the list above: ");
             String selector = sc.nextLine();
             System.out.println();
-            
+
             if (!result.next()) {
                 sb.append("NO DATA FOUND FOR " + selector);
             } else {
@@ -84,11 +137,13 @@ public class BrehmLab8A {
         Scanner sc = new Scanner(System.in);
         int choice = 0;
         while (choice < 1 || choice > 4) {
+            System.out.println();
+            System.out.println("Choose an option:");
             System.out.println("[1] Dump the contents of the database to console");
             System.out.println("[2] Get specific column of data");
             System.out.println("[3] Add a record");
             System.out.println("[4] Delete a record");
-            System.out.println("[5] Exit the program");
+            System.out.println("Enter anything else to quit.");
             choice = sc.nextInt();
         }
         return choice;
@@ -98,32 +153,44 @@ public class BrehmLab8A {
         ResultSet result = null;
         Connection dbCon = null;
         boolean exit = false;
+        boolean connected = false;
         try {
             Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
             String myDb = "jdbc:derby://localhost:1527/Lab8A";
             dbCon = DriverManager.getConnection(myDb, "nbuser", "nbuser");
             Statement statement = dbCon.createStatement();
             result = statement.executeQuery("SELECT * from Contractors");
+            connected = true;
         } catch (ClassNotFoundException | SQLException exc) {
             System.err.println(exc);
+            System.err.println("Connection failed. Please make sure the database server is running.");
         }
 
-        while (!exit) {
+        while (!exit && connected) {
             switch (menu()) {
                 case 1:
                     System.out.println();
                     System.out.print(listAll(result));
+                    break;
                 case 2:
                     System.out.println();
                     System.out.println(getData(result));
-                case 5:
+                    break;
+                case 3:
+                    System.out.println();
+                    addRecord(result, dbCon);
+                default:
                     exit = true;
+                    break;
             }
         }
-        try {
-            dbCon.close();
-        } catch (SQLException ex) {
-            Logger.getLogger(BrehmLab8A.class.getName()).log(Level.SEVERE, null, ex);
+
+        if (connected) {
+            try {
+                dbCon.close();
+            } catch (SQLException ex) {
+                System.err.println(ex);
+            }
         }
     }
 }
